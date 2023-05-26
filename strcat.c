e293889e6cd91bc45faac332132f353b3e1d8d8c
mamesareagan <mamesareagan0@gmai.com>
2023-05-26 04:14:03 -0700

    concatenation function

--- strcat.c
+++ strcat.c
@@ -4,18 +4,18 @@ char *_strcat(char *dest, char *source)
 {
 	char *des = dest;
 
-    	while (*des != '\0')
-    	{
+	while (*des != '\0')
+	{
 		des++;
-    	}	
+	}
 
 	while (*source != '\0')
 	{
 		*des = *source;
 		des++;
 		source++;
-    	}	
-    
+	}
+
 	*des = '\0';
 
 	return (dest);
