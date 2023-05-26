bc844072d8abc3cb089514a32c35f1ef5a386c3a
mamesareagan <mamesareagan0@gmai.com>
2023-05-26 13:18:26 -0700

    Reads a command from the user

--- read.c
+++ read.c
@@ -8,24 +8,24 @@
  */
 ssize_t read_command(char *buff, size_t size)
 {
-        ssize_t read = getline(&buff, &size, stdin);
+	ssize_t read = getline(&buff, &size, stdin);
 
-        if (read == -1)
-        {
-                if (feof(stdin))
-                {
-                        write(1, "\n", 1);
-                        exit(EXIT_SUCCESS);
-                }
-                else
+	if (read == -1)
+	{
+		if (feof(stdin))
 		{
-                        perror("read_command");
-                        exit(EXIT_FAILURE);
-                }
-        }
+			write(1, "\n", 1);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("read_command");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-        if (read > 0 && buff[read - 1] == '\n')
-                buff[read - 1] = '\0';
+	if (read > 0 && buff[read - 1] == '\n')
+		buff[read - 1] = '\0';
 
-        return (read);
+	return (read);
 }
