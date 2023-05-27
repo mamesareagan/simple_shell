a15677ce43852e408c1d660a79e91d8a97a16eb5
Sudireri <sudi.m@hotmail.com>
2023-05-27 11:37:47 -0700

    Simple shell functions

---
+++ errors.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int j = 0;
+
+	if (!str)
+		return;
+	while (str[j] != '\0')
+	{
+		_eputchar(str[j]);
+		j++;
+	}
+}
+
+/**
+ * _eputchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int j;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, j);
+		j = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[j++] = c;
+	return (1);
+}
+
+/**
+ * _putfdsc - writes the character c to given fd
+ * @c: The character to print
+ * @fdsc: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfdsc(char c, int fdsc)
+{
+	static int j;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || j >= WRITE_BUF_SIZE)
+	{
+		write(fdsc, buf, j);
+		j = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[j++] = c;
+	return (1);
+}
+
+/**
+ * _putsfdsc - prints an input string
+ * @str: the string to be printed
+ * @fdsc: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfdsc(char *str, int fdsc)
+{
+	int j = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		j += _putfdsc(*str++, fdsc);
+	}
+	return (j);
+}
+
---
+++ parser.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathsrt: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathsrt, int start, int stop)
+{
+	static char buf[1024];
+	int j = 0, l = 0;
+
+	for (l = 0, j = start; j < stop; j++)
+		if (pathsrt[j] != ':')
+			buf[l++] = pathsrt[j];
+	buf[l] = 0;
+	return (buf);
+}
+
+/**
+ * path_find - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathsrt: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *path_find(info_t *info, char *pathsrt, char *cmd)
+{
+	int j = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathsrt)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathsrt[j] || pathsrt[j] == ':')
+		{
+			path = dup_chars(pathsrt, curr_pos, j);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathsrt[j])
+				break;
+			curr_pos = j;
+		}
+		j++;
+	}
+	return (NULL);
+}
+
